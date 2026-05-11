7fe72f5761140cae9540a5a77fc90a9a5cb998c2
Jakob Stechow <jakob.stechow@pm.me>
2026-05-11 09:57:53 +0200

    fix: dont bind to loopback

--- crates/plugin/src/util.rs
+++ crates/plugin/src/util.rs
@@ -45,7 +45,7 @@ pub fn receive_all_packets_from_socket(socket: &UdpSocket) -> Vec<(Vec<u8>, Sock
 
 pub fn bind_socket(port: u16) -> UdpSocket {
     debug!("Binding socket on specified port {}", port);
-    let socket = UdpSocket::bind(format!("127.0.0.1:{}", port)).expect("Couldnt bind to address");
+    let socket = UdpSocket::bind(format!("0.0.0.0:{}", port)).expect("Couldnt bind to address");
     socket.set_nonblocking(true).unwrap();
     socket
 }
